f3ee1394dca040cb81770e1d04a267663bc7eba0
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:54:50 +0900

    drm/msm/sde: use kmem_cache pool for struct sde_fence
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: Ibb7f5b731e64118064535ece8d63a876fb758d94

--- techpack/display/msm/sde/sde_fence.c
+++ techpack/display/msm/sde/sde_fence.c
@@ -16,6 +16,8 @@
 #define SPEC_FENCE_FLAG_FENCE_ARRAY	0x10
 #define SPEC_FENCE_FLAG_ARRAY_BIND	0x11
 
+static struct kmem_cache *kmem_fence_pool;
+
 void *sde_sync_get(uint64_t fd)
 {
 	/* force signed compare, fdget accepts an int argument */
@@ -182,7 +184,7 @@ static void sde_fence_release(struct dma_fence *fence)
 	if (fence) {
 		f = to_sde_fence(fence);
 		kref_put(&f->ctx->kref, sde_fence_destroy);
-		kfree(f);
+		kmem_cache_free(kmem_fence_pool, f);
 	}
 }
 
@@ -235,7 +237,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 		goto exit;
 	}
 
-	sde_fence = kzalloc(sizeof(*sde_fence), GFP_KERNEL);
+	sde_fence = kmem_cache_zalloc(kmem_fence_pool, GFP_KERNEL);
 	if (unlikely(!sde_fence))
 		return -ENOMEM;
 
@@ -286,6 +288,8 @@ struct sde_fence_context *sde_fence_init(const char *name, uint32_t drm_id)
 		SDE_ERROR("failed to alloc fence ctx\n");
 		return ERR_PTR(-ENOMEM);
 	}
+	
+	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
 
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
@@ -306,6 +310,8 @@ void sde_fence_deinit(struct sde_fence_context *ctx)
 	}
 
 	kref_put(&ctx->kref, sde_fence_destroy);
+	
+	kmem_cache_destroy(kmem_fence_pool);
 }
 
 void sde_fence_prepare(struct sde_fence_context *ctx)
