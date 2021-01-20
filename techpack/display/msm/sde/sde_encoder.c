012e84ecb6e6b1980c0148a559afdcbf5dd14d27
Mahadevan <mahap@codeaurora.org>
2021-01-20 17:54:49 +0530

    techpack: disp: msm: sde: avoid rc restart when triggered from panel dead
    This change prevents scheduling of rc_restart in case of
    panel dead. This early return is required to avoid the list
    add corruption when a race condition happens between event
    thread and commit thread. When event thread is handling
    display failure notification if the virt_enable arrives in
    commit thread it will reinizialize kthread for
    delayed_off_work without deactivating the existing list
    which leads to linkage corruption.
    
    Signed-off-by: Mahadevan <mahap@codeaurora.org>
    Change-Id: I1940ee1236d816a86851985cacfb1315dd8cedd6
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    (cherry picked from commit 28aabc40ca0f01bb6b0ead040dd0e910bc790377)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -1877,6 +1877,10 @@ static void _sde_encoder_rc_restart_delayed(struct sde_encoder_virt *sde_enc,
 	if (sde_enc->delay_kickoff)
 		return;
 
+	/* return early if called from esd thread */
+	if (!sde_enc->delay_kickoff)
+		return;
+
 	/* set idle timeout based on master connector's lp value */
 	if (sde_enc->cur_master)
 		lp = sde_connector_get_lp(
