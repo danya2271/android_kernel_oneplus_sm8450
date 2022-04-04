61752d3073defd3b153a6ca2d84cb82bbf2b9cf4
Thomas Gleixner <tglx@linutronix.de>
2022-04-04 16:47:55 +0200

    timers: Simplify calc_index()
    The level granularity round up of calc_index() does:
    
       (x + (1 << n)) >> n
    
    which is obviously equivalent to
    
       (x >> n) + 1
    
    but compilers can't figure that out despite the fact that the input range
    is known to not cause an overflow. It's neither intuitive to read.
    
    Just write out the obvious.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/87h778j46c.ffs@tglx
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -508,7 +508,7 @@ static inline unsigned calc_index(unsigned long expires, unsigned lvl,
 	 * Round up with level granularity to prevent this.
 	 */
 	trace_android_vh_timer_calc_index(lvl, &expires);
-	expires = (expires + LVL_GRAN(lvl)) >> LVL_SHIFT(lvl);
+	expires = (expires >> LVL_SHIFT(lvl)) + 1;
 	*bucket_expiry = expires << LVL_SHIFT(lvl);
 	return LVL_OFFS(lvl) + (expires & LVL_MASK);
 }
