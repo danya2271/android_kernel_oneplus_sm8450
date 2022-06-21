d258dde8b72fd1ce7a0d48f687f9873c2fae3707
Dietmar Eggemann <dietmar.eggemann@arm.com>
2022-06-21 10:04:10 +0100

    sched, drivers: Remove max param from effective_cpu_util()/sched_cpu_util()
    effective_cpu_util() already has a `int cpu' parameter which allows to
    retrieve the CPU capacity scale factor (or maximum CPU capacity) inside
    this function via an arch_scale_cpu_capacity(cpu).
    
    A lot of code calling effective_cpu_util() (or the shim
    sched_cpu_util()) needs the maximum CPU capacity, i.e. it will call
    arch_scale_cpu_capacity() already.
    But not having to pass it into effective_cpu_util() will make the EAS
    wake-up code easier, especially when the maximum CPU capacity reduced
    by the thermal pressure is passed through the EAS wake-up functions.
    
    Due to the asymmetric CPU capacity support of arm/arm64 architectures,
    arch_scale_cpu_capacity(int cpu) is a per-CPU variable read access via
    per_cpu(cpu_scale, cpu) on such a system.
    On all other architectures it is a a compile-time constant
    (SCHED_CAPACITY_SCALE).
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Link: https://lkml.kernel.org/r/20220621090414.433602-4-vdonnefort@google.com
    [Sultan Alsawaf: backport to 5.10]
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2678,7 +2678,7 @@ enum schedutil_type {
 #if defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL) || defined(CONFIG_CPU_FREQ_GOV_SCHEDHORIZON)
 
 unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
+				 enum schedutil_type type,
 				 struct task_struct *p);
 
 static inline unsigned long cpu_bw_dl(struct rq *rq)
@@ -2709,7 +2709,7 @@ static inline unsigned long cpu_util_rt(struct rq *rq)
 }
 #else /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL || CONFIG_CPU_FREQ_GOV_SCHEDHORIZON */
 static inline unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
+				 enum schedutil_type type,
 				 struct task_struct *p)
 {
 	return 0;
