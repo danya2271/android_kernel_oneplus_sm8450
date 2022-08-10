ff491f5eada956bb8eb5fe2895c144310c1edf9a
Mel Gorman <mgorman@suse.de>
2022-08-10 10:44:38 +0100

    sched: Remove the limitation of WF_ON_CPU on wakelist if wakee cpu is idle (bnc#1189999 (Scheduler functional and performance backports)).
    Refresh
    - patches.suse/sched-core-Do-not-requeue-task-on-CPU-excluded-from-cpus_mask.patch
    
    suse-commit: 609dd6265e6459e9ddc1e5b2eb3a0ff2b9fbced9

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1829,7 +1829,6 @@ static inline int task_on_rq_migrating(struct task_struct *p)
 #define WF_SYNC			0x01		/* Waker goes to sleep after wakeup */
 #define WF_FORK			0x02		/* Child wakeup after fork */
 #define WF_MIGRATED		0x04		/* Internal use, task got migrated */
-#define WF_ON_CPU		0x08		/* Wakee is on_cpu */
 #define WF_ANDROID_VENDOR	0x1000		/* Vendor specific for Android */
 
 /*
