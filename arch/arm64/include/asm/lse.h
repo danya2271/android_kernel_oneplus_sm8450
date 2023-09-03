d0417a65701034b84896b42b81b5e4c62a1da508
Sultan Alsawaf <sultan@kerneltoast.com>
2023-09-03 12:14:57 -0700

    arm64: lse: Always use LSE atomics
    Since we are compiling for a single chipset that is known to support LSE,
    the system_uses_lse_atomics() static branch can be eliminated entirely.
    
    Therefore, make system_uses_lse_atomics() always true to always use LSE
    atomics.
    
    This reduces generated code size by removing LL/SC atomics, which improves
    instruction cache footprint.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -19,10 +19,8 @@
 extern struct static_key_false cpu_hwcap_keys[ARM64_NCAPS];
 extern struct static_key_false arm64_const_caps_ready;
 
-static inline bool system_uses_lse_atomics(void)
-{
-	return true;
-}
+/* Always use LSE atomics */
+#define system_uses_lse_atomics() true
 
 #define __lse_ll_sc_body(op, ...)		__lse_##op(__VA_ARGS__);
 
