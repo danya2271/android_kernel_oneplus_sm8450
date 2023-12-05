d20ac3e9618b7bd3affe2b8fd1f28c1db06d06cc
Youssef Esmat <youssefesmat@chromium.org>
2023-12-05 12:05:28 -0600

    BACKPORT: sched/eevdf: Also update slice on placement
    Tasks that never consume their full slice would not update their slice value.
    This means that tasks that are spawned before the sysctl scaling keep their
    original (UP) slice length.
    
    Fixes: 147f3efaa241 ("sched/fair: Implement an EEVDF-like scheduling policy")
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20230915124822.847197830@noisy.programming.kicks-ass.net
    
    BUG=b:308209790
    TEST=Manual tested on DUT
    
    Signed-off-by: Youssef Esmat <youssefesmat@chromium.org>
    
    Change-Id: Ia5637cbe1c227871144e1d171a45e6334fc9218b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5089902
    Reviewed-by: Vineeth Pillai <vineethrp@google.com>
    Commit-Queue: Youssef Esmat <youssefesmat@chromium.org>
    Tested-by: Vineeth Pillai <vineethrp@google.com>
    Reviewed-by: Suleiman Souhlal <suleiman@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4640,9 +4640,11 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq) {}
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
-	u64 vslice = calc_delta_fair(se->slice, se);
-	u64 vruntime = cfs_rq->min_vruntime;
+	u64 vslice, vruntime = cfs_rq->min_vruntime;
 	s64 lag = 0;
+
+	se->slice = sysctl_sched_base_slice;
+	vslice = calc_delta_fair(se->slice, se);
 
 	/*
 	 * Due to how V is constructed as the weighted average of entities,
