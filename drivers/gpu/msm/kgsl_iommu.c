4ffc27b355a88bd7f61df4eed5c632dd72b9e092
SIVA MULLATI <quic_smullati@quicinc.com>
2024-05-13 10:42:49 +0530

    kgsl: gmu: Send skip tlb hint for gmu domain
    Currently kgsl is not sending skip tlb hint to smmu
    for the gmu domain during slumber sequence. This may
    cause smmu to have vote on cx gdsc in slumber if any
    map/unmaps and results to cx wait timeouts.
    
    Change-Id: If8597d485dff82075fbaaa7b44bc349a48896fcb
    Signed-off-by: SIVA MULLATI <quic_smullati@quicinc.com>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -371,6 +371,7 @@ static size_t _iopgtbl_map_sg(struct kgsl_iommu_pt *pt, u64 gpuaddr,
 
 static void kgsl_iommu_send_tlb_hint(struct kgsl_mmu *mmu, bool hint)
 {
+	struct kgsl_device *device = KGSL_MMU_DEVICE(mmu);
 	struct kgsl_iommu *iommu = &mmu->iommu;
 
 	/*
@@ -388,6 +389,9 @@ static void kgsl_iommu_send_tlb_hint(struct kgsl_mmu *mmu, bool hint)
 	 */
 	if (!hint)
 		kgsl_iommu_flush_tlb(mmu);
+
+	/* TLB hint for GMU domain */
+	gmu_core_send_tlb_hint(device, hint);
 }
 
 static int
