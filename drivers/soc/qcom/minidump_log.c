ae351eeb308c15e5be9cc15757611da74789817e
danya2271 <dany2271@yandex.ru>
2024-07-19 21:54:26 +0500

    drivers: soc-qcom: minidump_log: Fix compilation with CONFIG_MODULES=n

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -1178,7 +1178,7 @@ static void md_register_panic_data(void)
 	md_debugfs_dmabufprocs(minidump_dir);
 }
 #endif
-
+#ifdef CONFIG_MODULES
 static int print_module(const char *name, void *mod_addr, void *data)
 {
 	if (!md_mod_info_seq_buf) {
@@ -1234,6 +1234,7 @@ static void md_register_module_data(void)
 
 	android_debug_for_each_module(print_module, NULL);
 }
+#endif
 
 #ifdef CONFIG_QCOM_MINIDUMP_PSTORE
 static void register_pstore_info(void)
@@ -1477,7 +1478,9 @@ int msm_minidump_log_init(void)
 #ifdef CONFIG_QCOM_MINIDUMP_FTRACE
 	md_register_trace_buf();
 #endif
+#ifdef CONFIG_MODULES
 	md_register_module_data();
+#endif
 #ifdef CONFIG_QCOM_MINIDUMP_PANIC_DUMP
 	md_register_panic_data();
 	atomic_notifier_chain_register(&panic_notifier_list, &md_panic_blk);
