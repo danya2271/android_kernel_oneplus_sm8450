42da99443f2e8f81901f96e3059b5614185a1da1
Tejun Heo <tj@kernel.org>
2024-08-05 12:39:10 -1000

    sched/fair: Make balance_fair() test sched_fair_runnable() instead of rq->nr_running
    balance_fair() skips newidle balancing if rq->nr_running - there are already
    tasks on the rq, so no need to try to pull tasks. This tests the total
    number of queued tasks on the CPU instead of only the fair class, but is
    still correct as the rq can currently only have fair class tasks while
    balance_fair() is running.
    
    However, with the addition of sched_ext below the fair class, this will not
    hold anymore and make put_prev_task_balance() skip sched_ext's balance()
    incorrectly as, when a CPU has only lower priority class tasks,
    rq->nr_running would still be positive and balance_fair() would return 1
    even when fair doesn't have any tasks to run.
    
    Update balance_fair() to use sched_fair_runnable() which tests
    rq->cfs.nr_running which is updated by bandwidth throttling. Note that
    pick_next_task_fair() already uses sched_fair_runnable() in its optimized
    path for the same purpose.
    
    Reported-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Chengming Zhou <chengming.zhou@linux.dev>
    Reviewed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://lore.kernel.org/r/ZrFUjlCf7x3TNXB8@slm.duckdns.org

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7491,7 +7491,7 @@ static void task_dead_fair(struct task_struct *p)
 static int
 balance_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 {
-	if (rq->nr_running)
+	if (sched_fair_runnable(rq))
 		return 1;
 
 	return newidle_balance(rq, rf) != 0;
