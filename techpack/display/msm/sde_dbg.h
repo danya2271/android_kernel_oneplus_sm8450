755d2508bbd664314c1c12bfeb72c45fa4d1eb1d
danya2271 <dany2271@yandex.ru>
2024-09-01 20:05:22 +0500

    techpack: display: Don't build debug bloat when CONFIG_DEBUG=n

--- techpack/display/msm/sde_dbg.h
+++ techpack/display/msm/sde_dbg.h
@@ -197,7 +197,7 @@ struct sde_dbg_reglog {
 	u32 enable;
 	u32 enable_mask;
 };
-
+#ifdef CONFIG_DEBUG
 extern struct sde_dbg_reglog *sde_dbg_base_reglog;
 
 /**
@@ -502,6 +502,315 @@ void sde_evtlog_set_filter(struct sde_dbg_evtlog *evtlog, char *filter);
  */
 int sde_evtlog_get_filter(struct sde_dbg_evtlog *evtlog, int index,
 		char *buf, size_t bufsz);
+#else
+extern struct sde_dbg_reglog *sde_dbg_base_reglog;
+
+/**
+ * SDE_REG_LOG - Write register write to the register log
+ */
+#define SDE_REG_LOG(blk_id, val, addr) sde_reglog_log(blk_id, val, addr)
+
+/**
+ * SDE_EVT32 - Write a list of 32bit values to the event log, default area
+ * ... - variable arguments
+ */
+#define SDE_EVT32(...) sde_evtlog_log(sde_dbg_base_evtlog, __func__, \
+__LINE__, SDE_EVTLOG_ALWAYS, ##__VA_ARGS__, \
+SDE_EVTLOG_DATA_LIMITER)
+
+/**
+ * SDE_EVT32_VERBOSE - Write a list of 32bit values for verbose event logging
+ * ... - variable arguments
+ */
+#define SDE_EVT32_VERBOSE(...) sde_evtlog_log(sde_dbg_base_evtlog, __func__, \
+__LINE__, SDE_EVTLOG_VERBOSE, ##__VA_ARGS__, \
+SDE_EVTLOG_DATA_LIMITER)
+
+/**
+ * SDE_EVT32_IRQ - Write a list of 32bit values to the event log, IRQ area
+ * ... - variable arguments
+ */
+#define SDE_EVT32_IRQ(...) sde_evtlog_log(sde_dbg_base_evtlog, __func__, \
+__LINE__, SDE_EVTLOG_IRQ, ##__VA_ARGS__, \
+SDE_EVTLOG_DATA_LIMITER)
+
+/**
+ * SDE_EVT32_EXTERNAL - Write a list of 32bit values for external display events
+ * ... - variable arguments
+ */
+#define SDE_EVT32_EXTERNAL(...) sde_evtlog_log(sde_dbg_base_evtlog, __func__, \
+__LINE__, SDE_EVTLOG_EXTERNAL, ##__VA_ARGS__, \
+SDE_EVTLOG_DATA_LIMITER)
+
+/**
+ * SDE_DBG_DUMP - trigger dumping of all sde_dbg facilities
+ * @dump_blk_mask: mask of all the hw blk-ids that has to be dumped
+ * @va_args:	list of named register dump ranges and regions to dump, as
+ *		registered previously through sde_dbg_reg_register_base and
+ *		sde_dbg_reg_register_dump_range.
+ *		Including the special name "panic" will trigger a panic after
+ *		the dumping work has completed.
+ */
+#define SDE_DBG_DUMP(dump_blk_mask, ...) sde_dbg_dump(SDE_DBG_DUMP_PROC_CTX, __func__, \
+dump_blk_mask, ##__VA_ARGS__, SDE_DBG_DUMP_DATA_LIMITER)
+
+/**
+ * SDE_DBG_DUMP_WQ - trigger dumping of all sde_dbg facilities, queuing the work
+ * @dump_blk_mask: mask of all the hw blk-ids that has to be dumped
+ * @va_args:	list of named register dump ranges and regions to dump, as
+ *		registered previously through sde_dbg_reg_register_base and
+ *		sde_dbg_reg_register_dump_range.
+ *		Including the special name "panic" will trigger a panic after
+ *		the dumping work has completed.
+ */
+#define SDE_DBG_DUMP_WQ(dump_blk_mask, ...) sde_dbg_dump(SDE_DBG_DUMP_IRQ_CTX, __func__, \
+dump_blk_mask, ##__VA_ARGS__, SDE_DBG_DUMP_DATA_LIMITER)
+
+/**
+ * SDE_DBG_DUMP_CLK_EN - trigger dumping of all sde_dbg facilities, without clk
+ * @dump_blk_mask: mask of all the hw blk-ids that has to be dumped
+ * @va_args:	list of named register dump ranges and regions to dump, as
+ *		registered previously through sde_dbg_reg_register_base and
+ *		sde_dbg_reg_register_dump_range.
+ *		Including the special name "panic" will trigger a panic after
+ *		the dumping work has completed.
+ */
+#define SDE_DBG_DUMP_CLK_EN(dump_blk_mask, ...) sde_dbg_dump(SDE_DBG_DUMP_CLK_ENABLED_CTX, \
+__func__, dump_blk_mask, ##__VA_ARGS__, SDE_DBG_DUMP_DATA_LIMITER)
+
+/**
+ * SDE_DBG_EVT_CTRL - trigger a different driver events
+ *  event: event that trigger different behavior in the driver
+ */
+#define SDE_DBG_CTRL(...) sde_dbg_ctrl(__func__, ##__VA_ARGS__, \
+SDE_DBG_DUMP_DATA_LIMITER)
+
+/**
+ * sde_mini_dump_add_va_region - add required va memory region to minidump.
+ * @size:	size of the memory region to be added
+ * @name:	minidump partition name
+ * @virt_addr:	pointer to memory region
+ * Returns:	none
+ */
+static void sde_mini_dump_add_va_region(const char *name, u32 size, void *virt_addr) {}
+
+/**
+ * sde_evtlog_init - allocate a new event log object
+ * Returns:	evtlog or -ERROR
+ */
+static struct sde_dbg_evtlog *sde_evtlog_init(void) {return 0;}
+
+/**
+ * sde_reglog_init - allocate a new reg log object
+ * Returns:	reglog or -ERROR
+ */
+static struct sde_dbg_reglog *sde_reglog_init(void) {return 0;}
+
+/**
+ * sde_evtlog_destroy - destroy previously allocated event log
+ * @evtlog:	pointer to evtlog
+ * Returns:	none
+ */
+static void sde_evtlog_destroy(struct sde_dbg_evtlog *evtlog) {}
+
+/**
+ * sde_reglog_destroy - destroy previously allocated reg log
+ * @reglog:	pointer to reglog
+ * Returns:	none
+ */
+static void sde_reglog_destroy(struct sde_dbg_reglog *reglog) {}
+
+/**
+ * sde_evtlog_log - log an entry into the event log.
+ *	log collection may be enabled/disabled entirely via debugfs
+ *	log area collection may be filtered by user provided flags via debugfs.
+ * @evtlog:	pointer to evtlog
+ * @name:	function name of call site
+ * @line:	line number of call site
+ * @flag:	log area filter flag checked against user's debugfs request
+ * Returns:	none
+ */
+static void sde_evtlog_log(struct sde_dbg_evtlog *evtlog, const char *name, int line,
+					int flag, ...) {}
+
+/**
+ * sde_reglog_log - log an entry into the reg log.
+ *      log collection may be enabled/disabled entirely via debugfs
+ *      log area collection may be filtered by user provided flags via debugfs.
+ * @reglog:     pointer to evtlog
+ * Returns:     none
+ */
+static void sde_reglog_log(u8 blk_id, u32 val, u32 addr) {}
+
+/**
+ * sde_evtlog_dump_all - print all entries in event log to kernel log
+ * @evtlog:	pointer to evtlog
+ * Returns:	none
+ */
+static void sde_evtlog_dump_all(struct sde_dbg_evtlog *evtlog) {}
+
+/**
+ * sde_evtlog_is_enabled - check whether log collection is enabled for given
+ *	event log and log area flag
+ * @evtlog:	pointer to evtlog
+ * @flag:	log area filter flag
+ * Returns:	none
+ */
+static bool sde_evtlog_is_enabled(struct sde_dbg_evtlog *evtlog, u32 flag) {return 0;}
+
+/**
+ * sde_evtlog_dump_to_buffer - print content of event log to the given buffer
+ * @evtlog:		pointer to evtlog
+ * @evtlog_buf:		target buffer to print into
+ * @evtlog_buf_size:	size of target buffer
+ * @update_last_entry:	whether or not to stop at most recent entry
+ * @full_dump:          whether to dump full or to limit print entries
+ * Returns:		number of bytes written to buffer
+ */
+static ssize_t sde_evtlog_dump_to_buffer(struct sde_dbg_evtlog *evtlog,
+								  char *evtlog_buf, ssize_t evtlog_buf_size,
+								  bool update_last_entry, bool full_dump) {return 0;}
+
+/**
+ * sde_dbg_init_dbg_buses - initialize debug bus dumping support for the chipset
+ * @hwversion:		Chipset revision
+ */
+static void sde_dbg_init_dbg_buses(u32 hwversion) {}
+
+/**
+ * sde_dbg_init - initialize global sde debug facilities: evtlog, regdump
+ * @dev:		device handle
+ * Returns:		0 or -ERROR
+ */
+static int sde_dbg_init(struct device *dev) {return 0;}
+
+/**
+ * sde_dbg_debugfs_register - register entries at the given debugfs dir
+ * @dev:	pointer to device
+ * Returns:	0 or -ERROR
+ */
+static int sde_dbg_debugfs_register(struct device *dev) {return 0;}
+
+/**
+ * sde_dbg_destroy - destroy the global sde debug facilities
+ * Returns:	none
+ */
+static void sde_dbg_destroy(void) {}
+
+/**
+ * sde_dbg_dump - trigger dumping of all sde_dbg facilities
+ * @queue_work:	whether to queue the dumping work to the work_struct
+ * @name:	string indicating origin of dump
+ * @dump_blk_mask: mask of all the hw blk-ids that has to be dumped
+ * @va_args:	list of named register dump ranges and regions to dump, as
+ *		registered previously through sde_dbg_reg_register_base and
+ *		sde_dbg_reg_register_dump_range.
+ *		Including the special name "panic" will trigger a panic after
+ *		the dumping work has completed.
+ * Returns:	none
+ */
+static void sde_dbg_dump(enum sde_dbg_dump_context mode, const char *name, u64 dump_blk_mask, ...) {}
+
+/**
+ * sde_dbg_ctrl - trigger specific actions for the driver with debugging
+ *		purposes. Those actions need to be enabled by the debugfs entry
+ *		so the driver executes those actions in the corresponding calls.
+ * @va_args:	list of actions to trigger
+ * Returns:	none
+ */
+static void sde_dbg_ctrl(const char *name, ...) {}
+
+/**
+ * sde_dbg_reg_register_base - register a hw register address section for later
+ *	dumping. call this before calling sde_dbg_reg_register_dump_range
+ *	to be able to specify sub-ranges within the base hw range.
+ * @name:	name of base region
+ * @base:	base pointer of region
+ * @max_offset:	length of region
+ * @phys_addr:	physical address of region
+ * @blk_id:	hw block id
+ * Returns:	0 or -ERROR
+ */
+static int sde_dbg_reg_register_base(const char *name, void __iomem *base,
+							  size_t max_offset, unsigned long phys_addr, u64 blk_id) {return 0;}
+
+/**
+ * sde_dbg_reg_register_cb - register a hw register callback for later
+ *	dumping.
+ * @name:	name of base region
+ * @cb:		callback of external region
+ * @cb_ptr:	private pointer of external region
+ * Returns:	0 or -ERROR
+ */
+static int sde_dbg_reg_register_cb(const char *name, void (*cb)(void *), void *ptr) {return 0;}
+
+/**
+ * sde_dbg_reg_unregister_cb - register a hw unregister callback for later
+ *	dumping.
+ * @name:	name of base region
+ * @cb:		callback of external region
+ * @cb_ptr:	private pointer of external region
+ * Returns:	None
+ */
+static void sde_dbg_reg_unregister_cb(const char *name, void (*cb)(void *), void *ptr) {}
+
+/**
+ * sde_dbg_reg_register_dump_range - register a hw register sub-region for
+ *	later register dumping associated with base specified by
+ *	sde_dbg_reg_register_base
+ * @base_name:		name of base region
+ * @range_name:		name of sub-range within base region
+ * @offset_start:	sub-range's start offset from base's base pointer
+ * @offset_end:		sub-range's end offset from base's base pointer
+ * @xin_id:		xin id
+ * Returns:		none
+ */
+static void sde_dbg_reg_register_dump_range(const char *base_name,
+									 const char *range_name, u32 offset_start, u32 offset_end,
+									 uint32_t xin_id) {}
+
+/**
+ * sde_dbg_dsi_ctrl_register - register a dsi ctrl for debugbus dumping
+ * @base:	iomem base address for dsi controller
+ * @name:	name of the dsi controller
+ * Returns:	0 or -ERROR
+ */
+static int sde_dbg_dsi_ctrl_register(void __iomem *base, const char *name) {return 0;}
+
+/**
+ * sde_dbg_set_sde_top_offset - set the target specific offset from mdss base
+ *	address of the top registers. Used for accessing debug bus controls.
+ * @blk_off: offset from mdss base of the top block
+ */
+static void sde_dbg_set_sde_top_offset(u32 blk_off) {}
+
+/**
+ * sde_dbg_set_hw_ownership_status - set the VM HW ownership status
+ * @enable:	flag to control HW ownership status
+ */
+static void sde_dbg_set_hw_ownership_status(bool enable) {}
+
+/**
+ * sde_evtlog_set_filter - update evtlog filtering
+ * @evtlog:	pointer to evtlog
+ * @filter:     pointer to optional function name filter, set to NULL to disable
+ */
+static void sde_evtlog_set_filter(struct sde_dbg_evtlog *evtlog, char *filter) {}
+
+/**
+ * sde_evtlog_get_filter - query configured evtlog filters
+ * @evtlog:	pointer to evtlog
+ * @index:	filter index to retrieve
+ * @buf:	pointer to output filter buffer
+ * @bufsz:	size of output filter buffer
+ * Returns:	zero if a filter string was returned
+ */
+static int sde_evtlog_get_filter(struct sde_dbg_evtlog *evtlog, int index,
+						  char *buf, size_t bufsz) {return 0;}
+
+
+
+#endif
 
 #ifndef CONFIG_DRM_SDE_RSC
 static inline void sde_rsc_debug_dump(u32 mux_sel)
