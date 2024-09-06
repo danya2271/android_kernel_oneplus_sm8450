f1e0333ba992d60d65f91e27ffcfbb50207c2bd1
danya2271 <dany2271@yandex.ru>
2024-09-06 15:11:26 +0500

    Revert "drivers: cpu_input_boost: Also enable prediction_disabled on boost"
    This reverts commit 269c924e8d56b84c166309c129985df863b9f063.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -150,7 +150,6 @@ static void boost_adjust_notify(struct cpufreq_policy *policy)
 
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
-		prediction_disabled = false;
 		sleep_disabled = false;
 		policy->min = get_idle_freq(policy);
 		policy->max = get_idle_freq(policy);
@@ -159,7 +158,6 @@ static void boost_adjust_notify(struct cpufreq_policy *policy)
 
 	/* Boost CPU to max frequency for max boost */
 	if (test_bit(MAX_BOOST, &b->state)) {
-		prediction_disabled = true;
 		sleep_disabled = true;
 		policy->min = get_max_boost_freq(policy);
 		policy->max = get_max_boost_freq(policy);
@@ -171,11 +169,9 @@ static void boost_adjust_notify(struct cpufreq_policy *policy)
 	 * unboosting, set policy->min to the absolute min freq for the CPU.
 	 */
 	if (test_bit(INPUT_BOOST, &b->state)) {
-		prediction_disabled = true;
 		sleep_disabled = true;
 		policy->min = get_input_boost_freq(policy);
 	} else {
-		prediction_disabled = false ;
 		sleep_disabled = false;
 		policy->min = get_min_freq(policy);
 	}
