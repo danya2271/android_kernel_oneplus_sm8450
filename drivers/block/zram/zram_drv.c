85c894fb4b18552f94f649525eec65a4fdb68e7c
danya2271 <dany2271@yandex.ru>
2024-11-03 02:32:55 +0500

    drivers: block-zram: Force zram size to 8 gb

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1794,7 +1794,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
+	disksize = (u64)8192 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 
