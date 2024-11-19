402defa1598a254df6e44ca5349be02069d68aba
danya2271 <dany2271@yandex.ru>
2024-11-19 19:35:48 +0500

    kernel: cpufreq_schedhorizon: Ramp up up_rate_limit for big cores

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -1087,7 +1087,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 1000;
+		tunables->up_rate_limit_us = 7700;
 		tunables->down_rate_limit_us = 500;
 	}
 
