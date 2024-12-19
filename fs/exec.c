e5756cbf17ca2808c634fee20ff7bae0a218b8a7
danya2271 <dany2271@yandex.ru>
2024-12-19 22:19:29 +0500

    fs: exec: Move hw composer to prime core

--- fs/exec.c
+++ fs/exec.c
@@ -83,6 +83,7 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware.display.composer-service"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct task_struct *zygote64_task;
 
@@ -1965,9 +1966,15 @@ static int do_execveat_common(int fd, struct filename *filename,
 
 	retval = bprm_execve(bprm, fd, filename, flags);
 
-	if (capable(CAP_SYS_ADMIN)) {
+	if (is_global_init(current->parent)) {
 		if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
                         zygote64_task = current;
+		else if (unlikely(!strncmp(filename->name,
+			HWCOMPOSER_BIN_PREFIX,
+			strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= IRQF_PERF_AFFINE | PF_PERF_CRITICAL | PF_NO_SETAFFINITY;
+			set_cpus_allowed_ptr(current, cpu_prime_mask);
+		}
 	}
 
 out_free:
