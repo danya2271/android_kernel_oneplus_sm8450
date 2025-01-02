90c88dbdea45e33fdbb8d6dcde494b998a353e8c
danya2271 <dany2271@yandex.ru>
2025-01-02 10:59:58 +0500

    drivers: Fix errors with -Wunused-result

--- drivers/misc/tri_state_key/oplus_tri_key.c
+++ drivers/misc/tri_state_key/oplus_tri_key.c
@@ -1205,7 +1205,8 @@ static ssize_t proc_hall_debug_info_write(struct file *file, const char __user *
 
 	if (count > 2)
 		return count;
-	copy_from_user(buf, buffer, count);
+	if (copy_from_user(buf, buffer, count))
+    	return -EFAULT;
 	if (!kstrtoint(buf, 0, &tmp))
 		tri_key_debug = tmp;
 	else
@@ -1244,7 +1245,8 @@ static ssize_t proc_hall_enable_irq_write(struct file *file, const char __user *
 
 	if (count > 2)
 		return count;
-	copy_from_user(buf, buffer, count);
+	if (copy_from_user(buf, buffer, count))
+    	return -EFAULT;
 	if (!kstrtoint(buf, 0, &tmp)) {
 		oplus_hall_enable_irq(0, tmp);
 		oplus_hall_enable_irq(1, tmp);
