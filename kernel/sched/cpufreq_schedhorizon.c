cd3871c04b9a3c0be98c0ee7294e77df8afc23dd
danya2271 <dany2271@yandex.ru>
2025-01-04 15:15:38 +0500

    Revert "kernel: sched: cpufreq_schedhorizon: Rework on headroom logic"
    This reverts commit 7d7b98e9d3c9cf78ef0a035fe8bb50f4cd1234cc.

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -441,17 +441,16 @@ unsigned long calculate_headroom_high(unsigned long headroom, int cpu, unsigned 
 
 static __always_inline
 unsigned long calculate_headroom_low(unsigned long headroom, int cpu, unsigned long util, int fps) {
-	if (util <= sysctl_util_low) { // check if util is way too high for decreasing headroom
+	if (sleep_disabled) { // check for touchboost
+		return util + (util >> 1);
+	} else if (util <= sysctl_util_low) { // check if util is way too high for decreasing headroom
 		if (cpumask_test_cpu(cpu, cpu_prime_mask))
 			return (util >> 3); // we want to reduce headroom of prime cluster if phone is idling with screen on
-		if (cpumask_test_cpu(cpu, cpu_perf_mask))
-			return (fps > sysctl_fps_threshold_high) ? (util >> 1) :
-			(fps < sysctl_fps_threshold_low) ? (util >> 3) :
-			(util >> 2);
-		return util + (util >> 1);
+		return (fps > sysctl_fps_threshold_high) ? (util - (util >> 1)) :
+		(fps < sysctl_fps_threshold_low) ? (util >> 3) :
+		(util >> 2);
 	} else {
-		return (fps < sysctl_fps_threshold_low) ? (util >> 3) :
-			util + (util >> 1);
+		return util;
 	}
 }
 
