4bedb465c63810654582549b4cbb0fe1de319b5c
danya2271 <dany2271@yandex.ru>
2025-01-06 12:22:08 +0500

    techpack: datarmnet-ext: Refactor code with clang-format
    Why qcom, whyyy

--- techpack/datarmnet-ext/wlan/rmnet_wlan_connection.h
+++ techpack/datarmnet-ext/wlan/rmnet_wlan_connection.h
@@ -10,11 +10,20 @@
 #include <linux/types.h>
 #include <linux/in6.h>
 #include "rmnet_wlan.h"
-struct DATARMNET0ca9d8ead7{union{__be32 DATARMNETdfe430c2d6;struct in6_addr 
-DATARMNET815cbb4bf5;};union{__be32 DATARMNET2cb607d686;struct in6_addr 
-DATARMNETc3f31215b7;};u8 DATARMNET0d956cc77a;};struct DATARMNET8d3c2559ca*
-DATARMNET07f0e0f286(struct DATARMNET0ca9d8ead7*DATARMNET54338da2ff);void 
-DATARMNETf4e1a29dbc(void);int DATARMNET9f106ed933(void);int DATARMNETf56cbaa2b1(
-void);
+struct DATARMNET0ca9d8ead7 {
+	union {
+		__be32 DATARMNETdfe430c2d6;
+		struct in6_addr DATARMNET815cbb4bf5;
+	};
+	union {
+		__be32 DATARMNET2cb607d686;
+		struct in6_addr DATARMNETc3f31215b7;
+	};
+	u8 DATARMNET0d956cc77a;
+};
+struct DATARMNET8d3c2559ca *
+DATARMNET07f0e0f286(struct DATARMNET0ca9d8ead7 *DATARMNET54338da2ff);
+void DATARMNETf4e1a29dbc(void);
+int DATARMNET9f106ed933(void);
+int DATARMNETf56cbaa2b1(void);
 #endif
-
