021b6f9b69431ac89c82f1f7a79759b7beedb8ab
danya2271 <dany2271@yandex.ru>
2025-01-11 14:54:40 +0500

    drivers:  Misc fixes

--- drivers/staging/qcacld-3.0/components/wmi/src/wmi_unified_roam_tlv.c
+++ drivers/staging/qcacld-3.0/components/wmi/src/wmi_unified_roam_tlv.c
@@ -2515,36 +2515,6 @@ extract_roam_sync_frame_event_tlv(wmi_unified_t wmi_handle, void *event,
 	return QDF_STATUS_SUCCESS;
 }
 
-static char *wmi_get_roam_event_reason_string(uint32_t reason)
-{
-	switch (reason) {
-	case WMI_ROAM_REASON_INVALID:
-		return "Default";
-	case WMI_ROAM_REASON_BETTER_AP:
-		return "Better AP";
-	case WMI_ROAM_REASON_BMISS:
-		return "BMISS";
-	case WMI_ROAM_REASON_LOW_RSSI:
-		return "Low Rssi";
-	case WMI_ROAM_REASON_SUITABLE_AP:
-		return "Suitable AP";
-	case WMI_ROAM_REASON_HO_FAILED:
-		return "Hand-off Failed";
-	case WMI_ROAM_REASON_INVOKE_ROAM_FAIL:
-		return "Roam Invoke failed";
-	case WMI_ROAM_REASON_RSO_STATUS:
-		return "RSO status";
-	case WMI_ROAM_REASON_BTM:
-		return "BTM";
-	case WMI_ROAM_REASON_DEAUTH:
-		return "Deauth";
-	default:
-		return "Invalid";
-	}
-
-	return "Invalid";
-}
-
 static enum roam_reason
 wmi_convert_fw_reason_to_cm_reason(uint32_t reason)
 {
@@ -2881,7 +2851,6 @@ extract_roam_stats_with_single_tlv(wmi_unified_t wmi_handle, uint8_t *evt_buf,
 				   struct roam_stats_event *stats_info)
 {
 	QDF_STATUS status;
-	uint8_t vdev_id = stats_info->vdev_id;
 
 	status = wmi_unified_extract_roam_11kv_stats(
 			wmi_handle, evt_buf, &stats_info->data_11kv[0], 0, 0);
