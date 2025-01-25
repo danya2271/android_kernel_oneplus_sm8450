228ea2795ebda76590580ae8bdcf9613399dc8ed
danya2271 <dany2271@yandex.ru>
2025-01-26 00:20:56 +0500

    kernel: sched: Tweak few values

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -1138,17 +1138,17 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
-		tunables->up_rate_limit_us = 3000;
+		tunables->up_rate_limit_us = 4500;
         tunables->down_rate_limit_us = 2000;
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
-		tunables->up_rate_limit_us = 4000;
+		tunables->up_rate_limit_us = 4500;
 		tunables->down_rate_limit_us = 2000;
 	}
 
 	if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
-		tunables->up_rate_limit_us = 2000;
+		tunables->up_rate_limit_us = 4500;
 		tunables->down_rate_limit_us = 2000;
 	}
 
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -54,8 +54,8 @@ static unsigned int normalized_sysctl_sched_base_slice	= 5000000ULL;
 unsigned int sysctl_sched_child_runs_first __read_mostly;
 unsigned int sysctl_fps_threshold_high __read_mostly = 45;
 unsigned int sysctl_fps_threshold_low __read_mostly = 25;
-unsigned int sysctl_headroom_big __read_mostly = 0;
-unsigned int sysctl_headroom_prime __read_mostly = 18;
+unsigned int sysctl_headroom_big __read_mostly = 3;
+unsigned int sysctl_headroom_prime __read_mostly = 3;
 unsigned int sysctl_util_low __read_mostly = 200;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 0UL;
