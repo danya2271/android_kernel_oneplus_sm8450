8a13cae1e47a9bbbaac9ac60f63798fe2e9782ec
danya2271 <dany2271@yandex.ru>
2025-05-25 21:17:29 +0500

    Revert "fs: Block various userspace bloatware services from running"
    This reverts commit 9fa8d09fb6cdd601ea749e4138542eb02ba6ebd7.

--- kernel/exit.c
+++ kernel/exit.c
@@ -787,7 +787,6 @@ void profile_task_exit(struct task_struct *tsk)
 }
 #endif
 
-void dead_special_task(void);
 void __noreturn do_exit(long code)
 {
 	struct task_struct *tsk = current;
@@ -796,8 +795,6 @@ void __noreturn do_exit(long code)
 	/* Trigger probing of device drivers when, e.g., modprobe exits */
 	if (IS_ENABLED(CONFIG_INTEGRATE_MODULES))
 		integrated_module_load_end();
-	
-	dead_special_task();
 
 	/*
 	 * We can get here from a kernel oops, sometimes with preemption off.
